0108ed0bc906ee7b59d0b6a5ca066426792a5d20
@evgenii_iurin <mmun6518@gmail.com>
2023-05-17 16:37:41 +0300

    database complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,7 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Main
 {
@@ -22,41 +19,66 @@ public class Main
         Root<Subscriptions> subscriptionsRoot = subscriptionsSelector.from(Subscriptions.class);
         subscriptionsSelector.select(subscriptionsRoot);
 
-        List<PurchaseList> purchaseListResults = session.getSession().createQuery(purchaseListSelector).getResultList();
-        Collections.sort(purchaseListResults,
-                (o1, o2) -> o1.getKey().getStudentName().compareTo(o2.getKey().getStudentName()));
-        List<Subscriptions> subscriptionsResults =  session.getSession().createQuery(subscriptionsSelector).getResultList();
-        Collections.sort(subscriptionsResults,
-                (o1, o2) -> o1.getStudent().getName().compareTo(o2.getStudent().getName()));
+        List<PurchaseList> purchaseListResults =
+                session.getSession().createQuery(purchaseListSelector).getResultList();
+        Collections.sort(purchaseListResults, (p1, p2) -> {
+            int cmp = p1.getKey().getStudentName().compareTo(p2.getKey().getStudentName());
+            if (cmp != 0) {
+                return cmp;
+            }
+            return Integer.valueOf(p1.getKey().getCourseName().compareTo(p2.getKey().getCourseName()));
+        });
+        List<Subscriptions> subscriptionsResults =
+                session.getSession().createQuery(subscriptionsSelector).getResultList();
+        Collections.sort(subscriptionsResults, (s1, s2) ->{
+                int cmp = s1.getStudent().getName().compareTo(s2.getStudent().getName());
+                if(cmp != 0) {
+                    return cmp;
+                }
+                return Integer.valueOf(s1.getCourse().getName().compareTo(s2.getCourse().getName()));
+                }
+                  );
 
-
-        purchaseListResults.forEach(purchaseList -> {
+        int index = 0;
+        for (PurchaseList purchaseList:
+             purchaseListResults) {
             LinkedPurchaseList linkedPurchaseList = new LinkedPurchaseList();
-            linkedPurchaseList.setId(getLinkedPurchaseListPK(subscriptionsResults, purchaseList));
+//            linkedPurchaseList.setId(getLinkedPurchaseListPK(subscriptionsResults, purchaseList));
+            LinkedPurchaseListPK linkedPurchaseListPK = new LinkedPurchaseListPK();
+            Subscriptions subscriptions = subscriptionsResults.get(index);
+            if(subscriptions.getStudent().getName().equals(purchaseList.getKey().getStudentName())
+                    && subscriptions.getCourse().getName().equals(purchaseList.getKey().getCourseName())
+            ) {
+                linkedPurchaseListPK.setCourseId(subscriptions.getCourse().getId());
+                linkedPurchaseListPK.setStudentId(subscriptions.getStudent().getId());
+            }
+            linkedPurchaseList.setId(linkedPurchaseListPK);
             linkedPurchaseList.setPurchaseList(purchaseList);
             linkedPurchaseList.setPrice(purchaseList.getPrice());
             session.getSession().merge(linkedPurchaseList);
-        });
+            index++;
+        }
 
         session.getTransaction().commit();
         session.getSession().close();
     }
 
-    private static LinkedPurchaseListPK getLinkedPurchaseListPK(List<Subscriptions> subscriptionsList, PurchaseList purchaseList) {
-        LinkedPurchaseListPK purchaseListPK = new LinkedPurchaseListPK();
-        for (Subscriptions sub:
-             subscriptionsList)
-        {
-            String studentName = purchaseList.getKey().getStudentName();
-            String courseName = purchaseList.getKey().getCourseName();
-            Date subscriptionDate = purchaseList.getKey().getSubscriptionDate();
-            if (sub.getStudent().equals(studentName) &&
-                    sub.getCourse().equals(courseName) &&
-                    sub.getSubscriptionDate().equals(subscriptionDate)) {
-                purchaseListPK.setStudentId(sub.getId().getStudentId().getId());
-                purchaseListPK.setCourseId(sub.getId().getCourseId().getId());
-            }
-        }
-        return purchaseListPK;
-    }
+//    private static LinkedPurchaseListPK
+//    getLinkedPurchaseListPK(List<Subscriptions> subscriptionsList, PurchaseList purchaseList)
+//    {
+//        LinkedPurchaseListPK purchaseListPK = new LinkedPurchaseListPK();
+//        for (Subscriptions sub:
+//             subscriptionsList)
+//        {
+//            String studentName = purchaseList.getKey().getStudentName();
+//            String courseName = purchaseList.getKey().getCourseName();
+//            Date subscriptionDate = purchaseList.getKey().getSubscriptionDate();
+//            if (sub.getStudent().equals(studentName) &&
+//                    sub.getCourse().equals(courseName)) {
+//                purchaseListPK.setStudentId(sub.getId().getStudentId().getId());
+//                purchaseListPK.setCourseId(sub.getId().getCourseId().getId());
+//            }
+//        }
+//        return purchaseListPK;
+//    }
 }
